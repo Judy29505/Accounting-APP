1d987fc76b16f2be9ea22ddb238d30e3c20d0e85
Judy29505 <1010136@korrnell.com.tw>
2017-11-05 15:32:26 +0800

    Add files via upload

---
+++ js/accounting.js
@@ -0,0 +1,18 @@
+var fdb = new ForerunnerDB();
+var db = fdb.db("accounting");
+var accountingCollection = db.collection('accounting');
+
+accountingCollection.load();
+
+
+function submit() {
+ var newAccounting = {
+    date : $("#date").val(),
+    category : $("#category").val(),
+    item : $("#item").val(),
+    cost : $("#cost").val()
+ }
+    accountingCollection.insert(newAccounting);
+    accountingCollection.save();
+
+}
